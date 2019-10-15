57f0a4c39a906db2d46cb0696d0bbb0268b87a02
范文华 <531362022@qq.com>
2019-10-15 10:16:10 +0800

    fix: Compatible with fs.copyFileSync() below node 8.5.0

--- src/commands/createJson.ts
+++ src/commands/createJson.ts
@@ -2,6 +2,7 @@
 
 import path from 'path';
 import fs from 'fs';
+import fse from 'fs-extra';
 import colors from 'colors';
 
 const targetFile = path.resolve('iconfont.json');
@@ -9,6 +10,7 @@ const targetFile = path.resolve('iconfont.json');
 if (fs.existsSync(targetFile)) {
   console.error(colors.red('File "iconfont.json" was created before.'));
 } else {
-  fs.copyFileSync(path.join(__dirname, '../libs/iconfont.json'), targetFile);
+  // fs.copyFileSync only can be used above node v8.5.0+
+  fse.copySync(path.join(__dirname, '../libs/iconfont.json'), targetFile);
   console.log(colors.green('File "iconfont.json" is created now. We recommend you add it to version control.'));
 }
