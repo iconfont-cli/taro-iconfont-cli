c0c4a197036b29518a1804f6bd6fa96b8d444b02
PandaJ <zjarek@me.com>
2022-04-20 15:06:09 +0800

    fix(alipay): 修复支付宝didUpdate拼写错误

--- snapshots/iconfont-ts/alipay/alipay.js
+++ snapshots/iconfont-ts/alipay/alipay.js
@@ -27,7 +27,7 @@ Component({
       });
     }
   },
-  disUpdate(prevProps) {
+  didUpdate(prevProps) {
     const size = this.props.size;
     const color = this.props.color;
 
@@ -45,7 +45,7 @@ Component({
     }
   },
   methods: {
-    fixColor: function() {
+    fixColor: function () {
       var color = this.props.color;
       var hex2rgb = this.hex2rgb;
 
@@ -57,7 +57,7 @@ Component({
         return item.indexOf('#') === 0 ? hex2rgb(item) : item;
       });
     },
-    hex2rgb: function(hex) {
+    hex2rgb: function (hex) {
       var rgb = [];
 
       hex = hex.substr(1);
@@ -66,7 +66,7 @@ Component({
         hex = hex.replace(/(.)/g, '$1$1');
       }
 
-      hex.replace(/../g, function(color) {
+      hex.replace(/../g, function (color) {
         rgb.push(parseInt(color, 0x10));
         return color;
       });
